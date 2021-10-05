6aea09a07daced8fb778c29943328a7979c1d817
VictorFujiyama <91971153+VictorFujiyama@users.noreply.github.com>
2021-10-05 13:56:23 -0300

    Update Fibonacci.py

--- Main program/Fibonacci.py
+++ Main program/Fibonacci.py
@@ -1,3 +1,7 @@
+# Escreva um script Python que recebe como parametro um número n inteiro positivo e imprime no console o n-ésimo valor correspondente da sequencia de Fibonacci. 
+# Obs.: O problema deve ser resolvido utilizando recursão.
+# fibonacci sequence = 0,1,1,2,3,5,8,13,21,34...
+
 def fibonacci(position):
     if position <= 1:
         return position
